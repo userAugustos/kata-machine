0ecb68b61b0278474e15b97a1c9e7404fae8f9c9
Felipe Augustos <felipe_augustos84@outlook.com.br>
2023-10-14 18:30:56 -0300

    feat: first algo: BinarySearchList, simplest version

--- src/algo/BinarySearchList.ts
+++ src/algo/BinarySearchList.ts
@@ -1,4 +1,20 @@
 
 export default function bs_list(haystack: number[], needle: number): boolean {
-	
+	let lo = 0
+	let hi = haystack.length
+
+	do {
+		const mid = Math.floor(lo + (hi - lo) /2)
+		const value = haystack[mid]
+
+		if(value === needle){
+			return true;
+		} else if(value > needle) {
+			hi = mid
+		} else { // (value < mid)
+			lo = mid + 1
+		}
+
+	} while (lo < hi);
+	return false
 }
