be27cd53c3dcf254d363d3ad3a633a5295b7cc6d
Felipe Augustos <felipe_augustos84@outlook.com.br>
2024-01-18 19:49:47 -0300

    feat: stack

--- src/algo/Stack.ts
+++ src/algo/Stack.ts
@@ -1,18 +1,46 @@
+interface Node<T> {
+    value: T,
+    prev?: Node<T>
+}
+
 export default class Stack<T> {
     public length: number;
+    private head?: Node<T>
 
     
 
     constructor() {
+        this.head = undefined
+        this.length = 0
     }
 
     push(item: T): void {
+        const node = { value: item } as Node<T>
+        this.length++;
 
-}
+        if(!this.head) {
+            this.head = node
+        }
+
+        node.prev = this.head;
+        this.head = node
+    }
+
     pop(): T | undefined {
+        // we are deleting a item, so the this.length goes -1 but never under 0
+        this.length = Math.max(0, this.length - 1)
+        if(this.length === 0) {
+            const head = this.head 
+            this.head = undefined
+            return head?.value
+        }
 
-}
+        const head = this.head as Node<T>
+        this.head = head.prev
+        return head.value
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value
+    }
 }
